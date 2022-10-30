36ccb29bc05c96edae54fdba875813b43e23d74d
saad <saad.moumou.coder@gmail.com>
2022-10-30 22:39:13 +0100

    fix inpute date time on js

--- scripts.js
+++ scripts.js
@@ -25,6 +25,6 @@ function showModalTask(id){
     form.task_type.value = document.getElementById(id).getAttribute('type');
     form.task_priority.value = document.getElementById(id).getAttribute('priorite');
     form.task_status.value = document.getElementById(id).getAttribute('statue');
-    //form.task_datetime.value = document.getElementById(id).getAttribute('datetime');
+    form.task_datetime.value = document.getElementById(id).getAttribute('datetime');
     form.task_description.value = document.getElementById(id).getAttribute('description');
 }
